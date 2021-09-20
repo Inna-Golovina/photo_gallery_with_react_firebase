4728309eabf3d2d28f69735b560c7a73cecc8589
Inna Golovina <igigig978@gmail.com>
2021-09-20 19:34:39 +0300

    useStorage

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,4 +1,4 @@
-/* import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { projectStorage, projectFirestore, timestamp } from '../firebase/config';
 
 const useStorage = (file) => {
@@ -7,15 +7,14 @@ const useStorage = (file) => {
   const [url, setUrl] = useState(null);
 
   useEffect(() => {
-    // references
     const storageRef = projectStorage.ref(file.name);
     const collectionRef = projectFirestore.collection('images');
-    
+
     storageRef.put(file).on('state_changed', (snap) => {
       let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
       setProgress(percentage);
-    }, (err) => {
-      setError(err);
+    }, (error) => {
+      setError(error);
     }, async () => {
       const url = await storageRef.getDownloadURL();
       const createdAt = timestamp();
@@ -24,15 +23,7 @@ const useStorage = (file) => {
     });
   }, [file]);
 
-  return { progress, url, error };
+  return {progress, url, error};
 }
 
-export default useStorage; */
-
-import { useState, useEffect } from 'react';
-import { projectStorage, projectFirestore, timestamp } from '../firebase/config';
-
-const useStorage = (file) => {
-  const [progress, setProgress] = useState(0);
-  const [] = useState(null);
-}
+export default useStorage;
