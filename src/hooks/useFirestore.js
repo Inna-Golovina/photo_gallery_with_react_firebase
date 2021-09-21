8e60ab1233a575d004b5187f9439e21406f97a34
Inna Golovina <igigig978@gmail.com>
2021-09-21 19:58:31 +0300

    Solution

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -3,21 +3,22 @@ import { projectFirestore } from '../firebase/config';
 
 const useFirestore = (collection) => {
   const [docs, setDocs] = useState([]);
-
-  useEffect(() => {
+  
+  useEffect (() => {
     const unsub = projectFirestore.collection(collection)
       .orderBy('createdAt', 'desc')
       .onSnapshot(snap => {
         let documents = [];
         snap.forEach(doc => {
-          documents.push({...doc.data(), id: doc.id});
+          documents.push({...doc.data(), id: doc.id });
         });
         setDocs(documents);
       });
 
-    return () => unsub();
+      return () => unsub();
     // this is a cleanup function that react will run when
     // a component using the hook unmounts
+
   }, [collection]);
 
   return { docs };
