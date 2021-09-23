fb4f25d9f01065f6c1a88381cd094bf0ceaee499
Inna Golovina <igigig978@gmail.com>
2021-09-23 18:41:44 +0300

    ImageGrid, UploadForm

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,2 +1,22 @@
+import React, { useEffect } from 'react';
+import useStorage from '../hooks/useStorage';
+import { motion } from 'framer-motion';
 
-import React from 'react';
+const ProgressBar = ({ file, setFile }) => {
+  const { progress, url } = useStorage(file);
+
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return (
+    <motion.div className="progress-bar"
+      initial={{ width: 0 }}
+      animate={{ width: progress + '%' }}
+    ></motion.div>
+  );
+} 
+
+export default ProgressBar;
