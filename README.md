22ad46bd4bb6d82c82129fbf6cc1deefd67032a6
Inna Golovina <igigig978@gmail.com>
2021-09-24 19:19:01 +0300

    fixed firebase

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Photo Gallery with React.js and Firebase
+
+https://Inna-Golovina.github.io/photo_gallery_with_react_firebase
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,15 @@
-/* import firebase from 'firebase/app'
-import 'firebase/storage';
-import 'firebase/firestore';
- */
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage';
 import 'firebase/compat/firestore';
 
-let firebaseConfig = {
-  apiKey: "AIzaSyDLmnoBvnjOQ-AaJgDw1DnVBiTETbcp8LI",
-  authDomain: "the-net-ninja-sandbox.firebaseapp.com",
-  databaseURL: "https://the-net-ninja-sandbox.firebaseio.com",
-  projectId: "the-net-ninja-sandbox",
-  storageBucket: "the-net-ninja-sandbox.appspot.com",
-  messagingSenderId: "485942827092",
-  appId: "1:485942827092:web:1811d9d8f1f5fabcd5b5c1"
+const firebaseConfig = {
+  apiKey: "AIzaSyDSNt3AUAtGKoiY6wWd6TrLBSFrDHzUGro",
+  authDomain: "photo-e54ed.firebaseapp.com",
+  projectId: "photo-e54ed",
+  storageBucket: "photo-e54ed.appspot.com",
+  messagingSenderId: "923639342760",
+  appId: "1:923639342760:web:171fb26ed63f497cbe449d",
+  measurementId: "G-01TY9E6XWF"
 };
 
 firebase.initializeApp(firebaseConfig);
