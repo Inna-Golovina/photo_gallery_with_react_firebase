1fdc66950b99822819d4bdf078825793e0e3f7d6
Inna Golovina <igigig978@gmail.com>
2021-09-24 19:21:00 +0300

    Updates

---
+++ precache-manifest.c073032e4579c59a951db0c3b0787be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30465ad5a9206cb7a187602f137fbc22",
+    "url": "/photo_gallery_with_react_firebase/index.html"
+  },
+  {
+    "revision": "76ce077b328d9ae22057",
+    "url": "/photo_gallery_with_react_firebase/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "d030adfe35ae83096e40",
+    "url": "/photo_gallery_with_react_firebase/static/js/2.64e8b72e.chunk.js"
+  },
+  {
+    "revision": "69580a4c0835108f39189e1d0a185609",
+    "url": "/photo_gallery_with_react_firebase/static/js/2.64e8b72e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ce077b328d9ae22057",
+    "url": "/photo_gallery_with_react_firebase/static/js/main.c63a0d99.chunk.js"
+  },
+  {
+    "revision": "f1002cb71a0028599ce8",
+    "url": "/photo_gallery_with_react_firebase/static/js/runtime-main.0e74318c.js"
+  }
+]);
